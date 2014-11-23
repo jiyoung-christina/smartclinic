86ee47fc624fc13775f32b866b7af6888c54bb9a
jiyoung kim <kjy8620@gmail.com>
2014-11-23 15:46:20 +0900

    서버 테스트

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,15 @@
 # -*- coding: utf-8 -*-
 import os
 
-from SmartClinicServ import app
+from SmartClinicServ import create_app
+
+import sys
+reload(sys)
+
+sys.setdefaultencoding("utf-8")
+
+application, socketio = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    socketio.run(host='127.0.0.1', port=port)
