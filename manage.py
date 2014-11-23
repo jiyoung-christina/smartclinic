5dedc900cf91d521ebd72872a0bb815ba5eda7e9
jiyoung kim <kjy8620@gmail.com>
2014-11-23 15:58:48 +0900

    no message

--- manage.py
+++ manage.py
@@ -12,11 +12,13 @@ sys.setdefaultencoding("utf-8")
 
 application = create_app()
 socketio = SocketIO(application)
-if __name__ == '__main__':
-     #application.run(host='0.0.0.0', port=5000, debug=True)
-    socketio.run(application)
 
 @socketio.on('message')
 def handle_message(message):
     print 'test socketio'
     print('received message: ' + message)
+
+if __name__ == '__main__':
+     #application.run(host='0.0.0.0', port=5000, debug=True)
+    socketio.run(application, host='0.0.0.0', port=5000)
+
