5503ffa50940230124cd807d8493182185db5960
jiyoungKim <kjy8620@gmail.com>
2014-11-23 14:16:02 +0900

    no message

--- manage.py
+++ manage.py
@@ -8,8 +8,8 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 application = create_app()
-
 socketio = SocketIO(application)
+
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=5000, debug=True)
     socketio(application)
