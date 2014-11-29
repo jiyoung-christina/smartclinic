e8e00b9c9f4bb38beef941732cb8046710542748
jiyoung kim <kjy8620@gmail.com>
2014-11-30 03:10:12 +0900

    데이터베이스 테이블 오류 수정

--- SmartClinicServ/model/reservation.py
+++ SmartClinicServ/model/reservation.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from SmartClinicServ.model import Base
 
 class Reservation(Base):
-    __tablename__ = 'reservatons'
+    __tablename__ = 'reservations'
     hosp_name = Column(String(100))
     email = Column(String(100))#foreign key add
     reserv_num = Column(Integer, primary_key=True, autoincrement=True)
